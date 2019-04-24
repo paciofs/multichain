ad14ff930f9ddba6e05132d9534f3eadf57de3b2
Robert Schmidtke <ro.schmidtke@gmail.com>
2019-04-24 10:13:13 +0200

    clone entire depot tools

--- .travis/fetch-v8.sh
+++ .travis/fetch-v8.sh
@@ -7,7 +7,7 @@ if [ ! -d "./v8build/v8" ]; then
   cd ./v8build
 
   # get the Google depot tools
-  git clone --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+  git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
   cd depot_tools
   git checkout af2ffd933d0e6d8b5bd8c48be7a2b2d568a5eea2
   cd ..
