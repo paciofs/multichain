958bab11269a8b6d40cec4931c15178d84016c11
Robert Schmidtke <ro.schmidtke@gmail.com>
2019-04-24 13:21:37 +0200

    checkout proper depot tools, ignore extra semicolon

--- .travis/fetch-v8.sh
+++ .travis/fetch-v8.sh
@@ -7,7 +7,10 @@ if [ ! -d "./v8build/v8" ]; then
   cd ./v8build
 
   # get the Google depot tools
-  git clone --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+  git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+  cd depot_tools
+  git checkout af2ffd933d0e6d8b5bd8c48be7a2b2d568a5eea2
+  cd ..
   export PATH=${PATH}:$(pwd)/depot_tools
 
   # obtain proper V8 version
@@ -21,6 +24,7 @@ if [ ! -d "./v8build/v8" ]; then
   INSERT_CFLAGS_LINE=$((CONFIG_DEFAULT_WARNINGS_LINE + IS_CLANG_LINE + 1))
   ex -s -c "${INSERT_CFLAGS_LINE}i|      \"-Wno-null-pointer-arithmetic\"," -c x build/config/compiler/BUILD.gn
   ex -s -c "${INSERT_CFLAGS_LINE}i|      \"-Wno-defaulted-function-deleted\"," -c x build/config/compiler/BUILD.gn
+  ex -s -c "${INSERT_CFLAGS_LINE}i|      \"-Wno-extra-semi\"," -c x build/config/compiler/BUILD.gn
 
   # the trace event repository is checked out at master, which does not compile currently
   # so use the version that was most likely used for 6.8.290
